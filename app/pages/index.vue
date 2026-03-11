90386b28d50098cb65dd087798b45157df21030e
MrCarbene <66732742+sx120609@users.noreply.github.com>
2026-03-11 14:46:27 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -68,7 +68,7 @@
         <div class="title-container">
           <h2 class="main-title">{{ siteTitle }}</h2>
           <div class="title-divider" />
-          <span class="sub-title">VoiceHub 校园广播系统</span>
+          <span class="sub-title">中国药科大学广播站</span>
         </div>
       </div>
 
